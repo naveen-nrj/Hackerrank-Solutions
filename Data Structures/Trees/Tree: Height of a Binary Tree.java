a9b2cb47715a7907bf1a02729920c72477c4ec67
amritsreekumar <amrit.sreekumar@gmail.com>
2017-12-12 11:49:40 +0530

    Added solutions for Trees

---
+++ Data Structures/Trees/Tree: Height of a Binary Tree.java
@@ -0,0 +1,20 @@
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+static int height(Node root){
+    int leftHeight = 0;
+    int rightHeight = 0;
+    if (root != null){
+        if (root.left != null){
+            leftHeight = height(root.left);
+        }
+        if (root.right != null){
+            rightHeight = height(root.right);
+        }
+    }
+
+    return leftHeight > rightHeight ? leftHeight+1 : rightHeight+1;
+}
