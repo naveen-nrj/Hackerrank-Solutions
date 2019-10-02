6de0bdd762be0b626fb4253272ce07ccd94786c6
Naveen Raj <naveenraj2493432@gmail.com>
2019-10-02 09:21:30 +0530

    Updated Scanner variable

--- Java/Introduction/Java Stdin and Stdout II.java
+++ Java/Introduction/Java Stdin and Stdout II.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
-        double d = scan.nextDouble();
-        scan.nextLine();
-        String s = scan.nextLine();
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        sc.nextLine();
+        String s = sc.nextLine();
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
