559fcf5aa62c4584cb22661e4fdef38772cc2502
Naveen Raj <naveenraj2493432@gmail.com>
2019-10-27 12:39:20 +0530

    Updated variable name

--- Java/Strings/Java Substring.java
+++ Java/Strings/Java Substring.java
@@ -9,9 +9,9 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String S = in.nextLine();
-        int start = in.nextInt();
+        int begin = in.nextInt();
         int end = in.nextInt(); 
-        for(int k=start;k<end;k++)
+        for(int k=begin;k<end;k++)
         {System.out.print(S.charAt(k));}
     }
 }
